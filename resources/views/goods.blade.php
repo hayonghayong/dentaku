9a8a132924145647b43ddb960f90725305cb59f7
Mayo Nakamura <mayosuzuki03@gmail.com>
2020-02-05 16:26:26 +0900

    ボタンをbtn-dangerに

--- resources/views/goods.blade.php
+++ resources/views/goods.blade.php
@@ -53,7 +53,7 @@
 <input type="text" name="name" class="form-control">
 重さ
 <input type="text" name="weight" class="form-control">
-<button type="submit" class="btn btn-info"> Save
+<button type="submit" class="btn btn-danger"> Save
 </button>
 </form>
 
